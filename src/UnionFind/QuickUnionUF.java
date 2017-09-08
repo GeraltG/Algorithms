2647b43233ce1b204987b33d9e69edd06a86f255
GCL <gucl713@gmail.com>
2017-09-08 22:14:29 +0800

    UnionFind

---
+++ src/UnionFind/QuickUnionUF.java
@@ -0,0 +1,28 @@
+package UnionFind;
+
+/**
+ * Created by gcl713 on 2017/9/8.
+ */
+public class QuickUnionUF {
+    private int[] id;
+    public  QuickUnionUF(int N)
+    {
+        id=new int[N];
+        for (int i=0;i<N;i++) id[i]=i;
+    }
+    private int root(int i)
+    {
+        while(i!=id[i]) i=id[i];
+        return i;
+    }
+    public boolean connected(int p,int q)
+    {
+        return root(p)==root(q);
+    }
+    public void union(int p,int q)
+    {
+        int i=root(p);
+        int j=root(q);
+        id[i]=i;
+    }
+}
